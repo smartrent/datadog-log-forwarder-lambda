ac18527096af482dcc655abea8ade0650c9cdd6e
Ryan Raub <>
2021-07-16 13:16:56 -1000

    adding new output for role arn

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_function_arn" {
   value = aws_lambda_function.logs_to_datadog.arn
 }
+
+output "lambda_iam_role_arn" {
+  value = aws_iam_role.lambda_execution.arn
+}
