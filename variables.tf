552daa172aa5e7613f777f7084c5e97a36b0b272
Stephan Fenton <stephan.fenton@gmail.com>
2022-10-25 10:43:04 -0700

    Incremented layer version numbers

--- variables.tf
+++ variables.tf
@@ -86,11 +86,11 @@ variable "layers" {
 variable "datadog_python_layer_version" {
   type        = number
   description = "The version of the Datadog Python Layer"
-  default     = 53
+  default     = 54
 }
 
 variable "datadog_extension_layer_version" {
   type        = number
   description = "The version of the Datadog Extension Layer"
-  default     = 22
+  default     = 23
 }
