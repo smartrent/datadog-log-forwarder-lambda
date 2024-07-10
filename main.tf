b8ed72b5c65320b656c19f397fe2224fc5886f16
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2024-07-10 14:49:04 -0400

    updates

--- main.tf
+++ main.tf
@@ -183,8 +183,9 @@ resource "aws_lambda_permission" "sns_topic_arns" {
 
 resource "aws_lambda_permission" "rds_logs" {
   count         = var.rds_logs ? 1 : 0
+  statement_id = "${local.account_id}-${var.aws_region}-rds-logs-to-datadog"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.logs_to_datadog.function_name
-  principal     = "logs.amazonaws.com"
+  principal     = "logs.${var.aws_region}.amazonaws.com"
   source_arn    = "arn:aws:logs:${var.aws_region}:${local.account_id}:log-group:/aws/rds/*:*"
 }
