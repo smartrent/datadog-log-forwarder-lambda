8588f6ec0f6f5990d8e2ac372596af488a17261c
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2024-09-12 10:15:45 -0400

    restrict region

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "kms_key_policy" {
     effect = "Allow"
     principals {
       type        = "Service"
-      identifiers = ["logs.amazonaws.com"]
+      identifiers = ["logs.${var.aws_region}.amazonaws.com"]
     }
     actions = [
       "kms:Encrypt",
